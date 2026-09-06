7b23a80f15266a7354b2843e56dc0804866f12a2
syedamunazza865-creator <syedamunazza865@gmail.com>
2026-09-06 16:46:55 +0530

    Add files via upload
    	Loop control keywords

---
+++ Section 4: Loops/break_continue_pass.py
@@ -0,0 +1,59 @@
+# for i in range(1,11):
+#     if i==5:
+#         break
+#     print(i)
+
+# count=0
+# while True:
+#     count+=1
+#     print("Count:"+str(count))
+
+#     if count==5:
+#         print("reached 5! breaking out..")
+#         break
+# print("outside the loop")
+
+
+# for i in range(1,11):
+#     if i==5:
+#         print("skipping 5")
+#         continue
+#     print(i)
+
+#printing odd numbers
+#--------------------
+
+# for i in range(1,11):
+#     if i%2==0:
+#         continue
+#     print(i)
+
+#diff bw break and continue
+
+# print("----break----")
+# for i in range(1,6):
+#     if i==3:
+#         break
+#     print(i)
+
+# print("----continue----")
+# for i in range(1,6):
+#     if i==3:
+#         continue
+#     print(i)
+
+#pass
+#----
+
+# for i in range(1,6):
+#     if i==3:
+#         pass
+#     print(i)
+
+for i in range(10):
+    pass
+    
+
+if True:
+    pass
+    
