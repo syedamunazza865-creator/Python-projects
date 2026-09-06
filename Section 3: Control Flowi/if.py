85478e842e7ee35e4ccdf939c7be43b30e085623
syedamunazza865-creator <syedamunazza865@gmail.com>
2026-09-06 16:41:26 +0530

    Add files via upload
    	Basic if with real examples

---
+++ Section 3: Control Flowi/if.py
@@ -0,0 +1,16 @@
+#if con:
+#    print()
+
+age=21
+if age>=18:
+    print("You are eligible to vote")
+print("Hello")
+
+pw="python123"
+if pw=="python122":
+    print("Access granted")
+
+
+marks=int(input("Enter marks:"))
+if marks>35:
+    print("Congratulation!,you passed") 
