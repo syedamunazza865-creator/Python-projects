a7460247a348f7a7f3e0eefe7f30708d58876d5d
syedamunazza865-creator <syedamunazza865@gmail.com>
2026-09-06 06:34:56 +0530

    Add files via upload
    Variables, int, float, str, bool

---
+++ Section 2: Python Basics/datatypes.py
@@ -0,0 +1,10 @@
+
+myname='Syeda'  #string
+age=29         #int
+height=5.6     #float
+is_student=False #boolean
+
+print(type(myname))
+print(type(age))
+print(type(height))
+print(type(is_student))
