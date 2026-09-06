e25537ee974a0dbbaea71f6dfa357f2e9fb69feb
syedamunazza865-creator <syedamunazza865@gmail.com>
2026-09-06 17:39:59 +0530

    Add files via upload

--- GUI/frame.py
+++ GUI/frame.py
@@ -1,1 +1,33 @@
+from tkinter import *
 
+root = Tk()
+root.geometry("300x150")
+
+w = Label(root, text='Chocos And IceCreams', font="50")
+w.pack()
+
+frame = Frame(root)
+frame.pack()
+
+bottomframe = Frame(root)
+bottomframe.pack(side=BOTTOM)
+
+b1_button = Button(frame, text="Choco", fg="red", bg="beige")
+b1_button.pack(side=LEFT)
+
+b2_button = Button(frame, text="Dark Choco", fg="brown", bg="beige")
+b2_button.pack(side=LEFT)
+
+b3_button = Button(frame, text="White Choco", fg="blue", bg="beige")
+b3_button.pack(side=LEFT)
+
+b4_button = Button(bottomframe, text="Pastry", fg="green", bg="yellow")
+b4_button.pack(side=BOTTOM)
+
+b5_button = Button(bottomframe, text="Cake", fg="green", bg="pink")
+b5_button.pack(side=BOTTOM)
+
+b6_button = Button(bottomframe, text="Tiramisu", fg="green", bg="cyan")
+b6_button.pack(side=BOTTOM)
+
+root.mainloop()
---
+++ GUI/spinbox.py
@@ -0,0 +1,9 @@
+
+from tkinter import *
+
+master = Tk()
+
+w = Spinbox(master, from_=0, to=10)
+w.pack()
+
+mainloop()
