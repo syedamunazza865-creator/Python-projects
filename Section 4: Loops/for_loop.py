ecfa33d64cd1d8933cc959ecc5b12f3903bd39f1
syedamunazza865-creator <syedamunazza865@gmail.com>
2026-09-06 16:45:40 +0530

    Add files via upload
    For loop with range and strings

--- Section 4: Loops/for_loop.py
+++ Section 4: Loops/for_loop.py
@@ -1,1 +1,31 @@
+# for i in range(1,16,2):
+#     print(i)
 
+#backward loop-->20-1
+# for count in range(20,0,-2):
+#     print(count)
+
+# #sum of numbers
+# total=0
+# for i in range(1,101):
+#     total=total+i
+# print("sum of 1 to 100:",str(total))
+
+# #Multiplication table
+# num=int(input("Enter a number:"))
+# for i in range(1,11):
+#     result=num*i
+#     print(str(num)+"x"+str(i)+"="+str(result))
+
+# name="Python"
+# for letter in name:
+#     print(letter)
+
+word=input("Enter a word:")
+vowels="aeiouAEIOU"
+count=0
+
+for letter in word:
+    if letter in vowels:
+        count=count+1
+print("Number of vowels:"+str(count))
