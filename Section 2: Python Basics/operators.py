f65ff2c884ced51499445631f0002b3341a522fb
syedamunazza865-creator <syedamunazza865@gmail.com>
2026-09-06 06:35:35 +0530

    Add files via upload
    	Arithmetic and comparison operators

---
+++ Section 2: Python Basics/operators.py
@@ -0,0 +1,30 @@
+#Arithmetic (+ - * / // % **)
+num1=12
+num2=3
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2) #quotient in float
+print(num1//num2) ##quotient in int
+print(num1%num2)  #remainder 
+print(num1**num2)
+#comparision (== != > < >= <=)
+print(num1==num2)
+print(num1!=num2)
+print(num1>num2)
+print(num1<num2)
+print(num1>=num2)
+print(num1<=num2)
+#logical ( and or not) 
+print(num1>15 or num1==12)
+print(not(True))
+#assignment (+= -= *= /= )
+a=5
+#a=a+5
+a+=5
+print(a)
+
+b=5
+#a=a+5
+b-=2
+print(b)
