6cd2f8d5a7205c7ca4442180963372ce0deca22f
syedamunazza865-creator <syedamunazza865@gmail.com>
2026-09-06 16:42:36 +0530

    Add files via upload
    	Multiple conditions, grades

---
+++ Section 3: Control Flowi/if_elif_else.py
@@ -0,0 +1,56 @@
+#Grade checker
+#-------------
+
+# marks=int(input("Enter marks:"))
+# if marks>=90:
+#     print("Grade A")
+# elif marks>=75:
+#     print("Grade B")
+# elif marks>=60:
+#     print("Grade C")
+# elif marks>=35:
+#     print("Grade D")
+# else:
+#     print("Grade F-Failed!")
+
+#Day of the week
+#-------------
+
+# day=int(input("Enter day number(1-7):"))
+
+# if day==1:
+#     print("Monday")
+# elif day==2:
+#     print("Tuesday")
+# elif day==3:
+#     print("Wenesday")
+# elif day==4:
+#     print("Thursday")
+# elif day==5:
+#     print("Friday")
+# elif day==6:
+#     print("Saturday")
+# elif day==7:
+#     print("Sunday")
+# else:
+#     print("Invalid day number")
+
+#BMI Calculator
+#--------------
+
+# bmi=float(input("Enter your BMI:"))
+# if bmi<18.5:
+#     print("underweight")
+# elif bmi<25:
+#     print("Normal weight")
+# elif bmi<30:
+#     print("Overweight")
+# else:
+#     print("obese")
+
+marks=95
+if marks>=90:
+    print("Excellent")
+if marks>=75:
+    print("Good!")
+print("----------")
