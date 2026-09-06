cfe2da415357916b2bc3e0d23fb6c8d1357700f0
syedamunazza865-creator <syedamunazza865@gmail.com>
2026-09-06 06:36:50 +0530

    Add files via upload
    	input() and type conversion

---
+++ Section 2: Python Basics/input.py
@@ -0,0 +1,9 @@
+#type conversion
+name=input("What is your name?")
+age=int(input("How old are you?"))
+marks=float(input("How much did u score?"))
+sports=bool(input("Do u like sports"))
+print(type(name))
+print(type(age))
+print(type(marks))
+print(type(sports))
