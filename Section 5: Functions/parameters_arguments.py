9d401c0b17dc02a20ffdc45e4798d203691843f8
syedamunazza865-creator <syedamunazza865@gmail.com>
2026-09-06 16:50:44 +0530

    Add files via upload
    	Single and multiple parameters

---
+++ Section 5: Functions/parameters_arguments.py
@@ -0,0 +1,35 @@
+# def greeting(name):  #parameter
+#     print("Have a nice day!",name)
+# greeting("Rahul")    #argument
+# greeting("ajay")
+
+#square
+
+# def square(number):
+#     result=number*number
+#     print(str(number)+" sqaured is "+str(result))
+
+# square(4)
+# square(7)
+
+# def print_stars(count):
+#     print("*"*count)
+
+# print_stars(5)
+# print_stars(10)
+
+# def adding(a,b):
+#     total=a+b
+#     print(str(a)+ " + "+str(b)+ " = " + str(total))
+
+# adding(5,3)
+# adding(200,500)
+
+def student_info(name,age,course):
+    print("=====================")
+    print("Name:",name)
+    print("Age:",age)
+    print("Course:",course)
+    print("=====================")
+student_info("Rahul",20)
+student_info("Priya",22,"BTech")
