a9948410c0d2bef17f8dccc84a842eaa34ad8474
syedamunazza865-creator <syedamunazza865@gmail.com>
2026-09-06 06:33:45 +0530

    Add files via upload
    Variables, int, float, str, bool

---
+++ Section 2: Python Basics/variable.py
@@ -0,0 +1,10 @@
+""" A variable is like a container that stores information. Think of it like a labeled box — you put something inside it, and you can use that label to access it later. """
+#var_name=data
+
+name='Syeda'
+age=29
+height=5.6
+is_student=True
+
+print("name"," is ",age,"years old")
+print("she is ",height," inches tall")
